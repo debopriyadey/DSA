5b69569d1f09017e194f7918f4f2299ef5840098
debopriyadey <debodey158@gmail.com>
2021-01-28 02:24:43 +0530

    added sieve of eranthenes

--- Algorithm/sieveOfEratosthenes.cpp
+++ Algorithm/sieveOfEratosthenes.cpp
@@ -1,2 +1,37 @@
 #include<bits/stdc++.h>
+
 using namespace std;
+
+void SieveOfEratosthenes(int n){
+	
+	bool prime[n+1] = {true};
+	memset(prime, true, sizeof(prime));
+	for(int i=2; i*i<=n; i++)
+	{
+		if(prime[i]==true)
+		{
+			for(int j=i*i; j<=n; j+=i)
+			{
+				prime[j]= false;
+			}
+		}
+	}
+	// Print all prime numbers
+    for (int p = 2; p <= n; p++)
+    {
+        if (prime[p])
+        {
+            cout << p << " ";
+        }
+    }
+}
+ 
+// Driver Code
+int main()
+{
+    int n = 30;
+    cout << "Following are the prime numbers smaller "
+         << " than or equal to " << n << endl;
+    SieveOfEratosthenes(n);
+    return 0;
+}
