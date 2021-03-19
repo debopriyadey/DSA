b900d7fec4194e6eec2e5e5b804a8518d5b33a3a
Debopriya Dey <64556550+debopriyadey@users.noreply.github.com>
2021-03-20 00:36:22 +0530

    Update floyd’sCycle-FindingAlgorithm.cpp

--- Algorithm/floyd’sCycle-FindingAlgorithm.cpp
+++ Algorithm/floyd’sCycle-FindingAlgorithm.cpp
@@ -1,1 +1,36 @@
+// C++ Program for detecting loop in a linked list
+// we use two pointer in this method
+// slow is increased by 1 and fast is increased by 2
+
+/*struct Node
+{
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+
+*/
+
+class Solution
+{
+    public:
+    bool detectLoop(Node* head)
+    {
+        // your code here
+        struct Node *slow = head;
+        struct Node *fast = head;
+        while(slow && fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
 
