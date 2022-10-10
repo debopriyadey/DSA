cf4f6646d7d0d76b708319fc204414e5dcb5ccde
Pratham Jha <prathamjha5683@gmail.com>
2022-10-10 10:32:08 +0530

    Heap Sort in  c++ added

--- Algorithm/HeapSort.cpp
+++ Algorithm/HeapSort.cpp
@@ -1,51 +1,35 @@
-// Heap Sort in C
+// C++ program for implementation of Heap Sort
 
-#include <stdio.h>
-
-// Function to swap the position of two elements
-
-void swap(int* a, int* b)
-{
-
-	int temp = *a;
-
-	*a = *b;
-
-	*b = temp;
-}
+#include <iostream>
+using namespace std;
 
 // To heapify a subtree rooted with node i
 // which is an index in arr[].
 // n is size of heap
 void heapify(int arr[], int N, int i)
 {
-	// Find largest among root, left child and right child
 
 	// Initialize largest as root
 	int largest = i;
 
 	// left = 2*i + 1
-	int left = 2 * i + 1;
+	int l = 2 * i + 1;
 
 	// right = 2*i + 2
-	int right = 2 * i + 2;
+	int r = 2 * i + 2;
 
 	// If left child is larger than root
-	if (left < N && arr[left] > arr[largest])
-
-		largest = left;
+	if (l < N && arr[l] > arr[largest])
+		largest = l;
 
 	// If right child is larger than largest
 	// so far
-	if (right < N && arr[right] > arr[largest])
+	if (r < N && arr[r] > arr[largest])
+		largest = r;
 
-		largest = right;
-
-	// Swap and continue heapifying if root is not largest
 	// If largest is not root
 	if (largest != i) {
-
-		swap(&arr[i], &arr[largest]);
+		swap(arr[i], arr[largest]);
 
 		// Recursively heapify the affected
 		// sub-tree
@@ -57,18 +41,18 @@ void heapify(int arr[], int N, int i)
 void heapSort(int arr[], int N)
 {
 
-	// Build max heap
+	// Build heap (rearrange array)
 	for (int i = N / 2 - 1; i >= 0; i--)
-
 		heapify(arr, N, i);
 
-	// Heap sort
-	for (int i = N - 1; i >= 0; i--) {
+	// One by one extract an element
+	// from heap
+	for (int i = N - 1; i > 0; i--) {
 
-		swap(&arr[0], &arr[i]);
+		// Move current root to end
+		swap(arr[0], arr[i]);
 
-		// Heapify root element to get highest element at
-		// root again
+		// call max heapify on the reduced heap
 		heapify(arr, i, 0);
 	}
 }
@@ -76,9 +60,9 @@ void heapSort(int arr[], int N)
 // A utility function to print array of size n
 void printArray(int arr[], int N)
 {
-	for (int i = 0; i < N; i++)
-		printf("%d ", arr[i]);
-	printf("\n");
+	for (int i = 0; i < N; ++i)
+		cout << arr[i] << " ";
+	cout << "\n";
 }
 
 // Driver's code
@@ -89,8 +73,7 @@ int main()
 
 	// Function call
 	heapSort(arr, N);
-	printf("Sorted array is\n");
+
+	cout << "Sorted array is \n";
 	printArray(arr, N);
 }
-
-// This code is contributed by _i_plus_plus_.
